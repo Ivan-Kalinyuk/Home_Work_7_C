c20f09954675d1687471cdcc21b93540c653fb09
Ivan_Kalinyuk <ivan_kalinyuk01@mail.ru>
2022-09-04 08:22:52 +0300

    Решили №47

---
+++ №47/Program.cs
@@ -0,0 +1,29 @@
+﻿void PrintArray(double[,] matr )
+{
+    for (int i=0; i<matr.GetLength(0); i++)
+    {
+        for (int j=0; j<matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}  ");
+        }
+    Console.WriteLine( );
+    }
+}
+Random r = new Random();
+void FillArray(double[,] matr )
+{
+    for (int i=0; i<matr.GetLength(0); i++)
+    {
+        for (int j=0; j<matr.GetLength(1); j++)
+        {
+            matr [i,j] = r.Next(-100000, 100000) / 10.00;
+        }
+    }
+}
+Console.WriteLine ("Введите количество строк Вашего массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите количество столбцов Вашего массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double [,] matrix = new double [m,n];
+FillArray(matrix);
+PrintArray(matrix);
